b940dedbdb58603ed6d9c278c5b5906eef057663
harshit466 <161843107+harshit466@users.noreply.github.com>
2024-03-14 22:43:42 +0530

    Delete src/com/learning/core/day4/NotEligibleException.java

--- src/com/learning/core/day4/NotEligibleException.java
+++
@@ -1,7 +0,0 @@
-package com.learning.core.day4.D04P01;
-
-public class NotEligibleException extends RuntimeException {
-    public NotEligibleException(String message) {
-        super(message);
-    }
-}
