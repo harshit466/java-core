f0a4e5d201f37e6967de3e44a64f67133a0862ce
harshit466 <161843107+harshit466@users.noreply.github.com>
2024-03-14 22:33:20 +0530

    Delete src/com/learning/core/day1/Ointment.java

--- src/com/learning/core/day1/Ointment.java
+++
@@ -1,8 +0,0 @@
-package com.learning.core.day1.D01P02;
-
-class Ointment implements MedicineInfo {
-    @Override
-    public void displayLabel() {
-        System.out.println("Ointment is for external use only.");
-    }
-}
