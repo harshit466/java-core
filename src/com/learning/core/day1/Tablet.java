1843e4cfe4b11a56ab719492d7665868afa78936
harshit466 <161843107+harshit466@users.noreply.github.com>
2024-03-14 22:32:52 +0530

    Delete src/com/learning/core/day1/Tablet.java

--- src/com/learning/core/day1/Tablet.java
+++
@@ -1,8 +0,0 @@
-package com.learning.core.day1.D01P02;
-
-class Tablet implements MedicineInfo {
-	    @Override
-	    public void displayLabel() {
-	        System.out.println("Store Tablets in a cool dry place.");
-	    }
-	}
