536ae4423d25413f51cc496fb872daa1175b12bd
harshit466 <161843107+harshit466@users.noreply.github.com>
2024-03-14 22:45:42 +0530

    Add files via upload

---
+++ src/com/learning/core/day4/D04P02.java
@@ -0,0 +1,32 @@
+package com.learning.core.day4;
+
+import java.util.Scanner;
+
+public class D04P02 {
+	
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.print("Enter player name: ");
+	        String playerName = scanner.next();
+	        System.out.print("Enter critic 1 rating: ");
+	        float critic1 = scanner.nextFloat();
+	        System.out.print("Enter critic 2 rating: ");
+	        float critic2 = scanner.nextFloat();
+	        System.out.print("Enter critic 3 rating: ");
+	        float critic3 = scanner.nextFloat();
+
+	        CricketRating rating = new CricketRating(playerName, critic1, critic2);
+	        rating.calculateAverageRating(critic1, critic2, critic3);
+
+	        try {
+	            if (critic3 == 0.0f) {
+	                throw new NotEligibleException("NotEligible");
+	            }
+	            rating.calculateCoins();
+	            rating.display();
+	        } catch (NotEligibleException e) {
+	            System.out.println(e.getMessage());
+	        }
+	    }
+	}
+
