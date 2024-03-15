b904ba65de7ff7627a68758a8a1e0134103c109c
harshit466 <161843107+harshit466@users.noreply.github.com>
2024-03-15 18:17:53 +0530

    Add files via upload

---
+++ src/com/learning/core/day8/D08P02.java
@@ -0,0 +1,25 @@
+package com.learning.core.day8;
+import java.util.Scanner;
+
+public class D08P02 {
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter elements for the queue :");
+        for(int i=0;i<5;i++)
+        {
+        	int input=scanner.nextInt();
+        	queue.enqueue(input);
+        }
+       
+        System.out.print("Elements in queue: ");
+        queue.display();
+
+        queue.dequeue();
+        queue.dequeue();
+
+        System.out.print("After removing two elements: ");
+        queue.display();
+    }
+}
